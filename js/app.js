7df9fcde1056e711dbcc8a4a559c9e7e5c28f777
vitorjoseteixeirasebastiao <vitorjoseteixeirasebastiao@gmail.com>
2026-01-28 19:29:57 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,28 +1,7 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs, onSnapshot } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
-
-window.onload = async function() {
-
-  // ===== Firebase =====
-  const firebaseConfig = {
-    apiKey: "AIzaSyByYEISjGfRIh7Xxx5j7rtJ7Fm_nmMTgRk",
-    authDomain: "vpm2026-8167b.firebaseapp.com",
-    projectId: "vpm2026-8167b",
-    storageBucket: "vpm2026-8167b.appspot.com",
-    messagingSenderId: "129557498750",
-    appId: "1:129557498750:web:c2a510c04946583a17412f"
-  };
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
-
-  function mostrarMensagem(msg){
-    const div = document.getElementById("mensagens");
-    div.innerText = msg;
-    setTimeout(()=> div.innerText="", 3000);
-  }
+window.onload = function() {
 
   // ===== Mapa =====
-  const map = L.map("map").setView([0,0], 15);
+  const map = L.map("map").setView([0,0],15);
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "© OpenStreetMap"
   }).addTo(map);
@@ -37,95 +16,22 @@ window.onload = async function() {
 
   const marcadorUsuario = L.marker([0,0], {icon:iconeUsuario}).addTo(map);
 
-  let posicaoAtual = null;
   let primeiraLocalizacao = true;
 
-  // ===== Atualiza posição do usuário =====
   if(navigator.geolocation){
     navigator.geolocation.watchPosition(pos=>{
-      posicaoAtual = {lat: pos.coords.latitude, lng: pos.coords.longitude};
-      marcadorUsuario.setLatLng([posicaoAtual.lat,posicaoAtual.lng]);
+      const lat = pos.coords.latitude;
+      const lng = pos.coords.longitude;
+      marcadorUsuario.setLatLng([lat,lng]);
       if(primeiraLocalizacao){
-        map.setView([posicaoAtual.lat,posicaoAtual.lng],18);
+        map.setView([lat,lng],18);
         primeiraLocalizacao=false;
       }
     }, err=>{
-      mostrarMensagem("Erro GPS: "+err.message);
+      console.error("Erro GPS:", err.message);
     }, {enableHighAccuracy:true});
   } else {
-    mostrarMensagem("GPS não disponível");
+    console.error("GPS não disponível");
   }
-
-  // ===== Referência Firebase =====
-  const colRef = collection(db,"teste");
-  const markers = {};
-
-  // ===== Carrega marcadores existentes =====
-  const docsExistentes = await getDocs(colRef);
-  docsExistentes.forEach(docSnap=>{
-    const id = docSnap.id;
-    const data = docSnap.data();
-    markers[id] = L.marker([data.latitude,data.longitude])
-                    .addTo(map)
-                    .bindPopup("Marcador existente");
-  });
-
-  // ===== Atualização em tempo real =====
-  onSnapshot(colRef, snapshot=>{
-    snapshot.docChanges().forEach(change=>{
-      const id = change.doc.id;
-      const data = change.doc.data();
-      if(change.type==="added" && !markers[id]){
-        markers[id] = L.marker([data.latitude,data.longitude])
-                        .addTo(map)
-                        .bindPopup("Marcador existente");
-      }
-    });
-  });
-
-  // ===== Botão adicionar marcador =====
-  const btnMarcador = document.getElementById("btnMarcador");
-  btnMarcador.disabled = true; // bloqueia até ter posição
-  const habilitarBtn = setInterval(()=>{
-    if(posicaoAtual){
-      btnMarcador.disabled = false;
-      clearInterval(habilitarBtn);
-    }
-  },100);
-
-  btnMarcador.onclick = async ()=>{
-    if(!posicaoAtual) return;
-    try{
-      const docRef = await addDoc(colRef,{
-        latitude: posicaoAtual.lat,
-        longitude: posicaoAtual.lng,
-        data: new Date()
-      });
-      // adiciona marcador imediatamente
-      markers[docRef.id] = L.marker([posicaoAtual.lat,posicaoAtual.lng])
-                           .addTo(map)
-                           .bindPopup("Marcador criado");
-      mostrarMensagem("Marcador salvo!");
-    } catch(err){
-      console.error(err);
-      mostrarMensagem("Erro ao salvar no Firebase");
-    }
-  };
-
-  // ===== Botão centralizar no usuário =====
-  const btnCentralizar = document.getElementById("btnCentralizar");
-  btnCentralizar.disabled = true;
-  const habilitarCentralizar = setInterval(()=>{
-    if(posicaoAtual){
-      btnCentralizar.disabled = false;
-      clearInterval(habilitarCentralizar);
-    }
-  },100);
-
-  btnCentralizar.onclick = ()=>{
-    if(posicaoAtual){
-      map.setView([posicaoAtual.lat,posicaoAtual.lng],18);
-    }
-  };
 
 };
